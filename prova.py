15cf30167ae57a9ca8d6b834c0b7b743b00eaad7
laviniafranqueiro <133999323+laviniafranqueiro@users.noreply.github.com>
2023-06-16 10:25:15 -0300

    Create prova.py

---
+++ prova.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Notebooks e produtos relacionados')
+st.caption('Lavínia Franqueiro')
+
+user_input = st.text_input("Qual produto você deseja?")
+
+df = pd.read_csv('jupyter2.py.csv') 
+df
+
+# Check if user has entered something in the text input box
+if user_input:
+    # Filter the DataFrame based on the user input
+    df = df[df['marca'].str.contains(user_input, case=False)]
+
+for i in range(df.shape[0]):
+    marca = df.iloc[i]['marca']
+    preco = df.iloc[i]['preço']
+    link = df.iloc[i]['link']
+    st.markdown(f"**Marca**: {marca}  \n**Preço**: {preco}  \n**[Link para o produto]({link})**")
